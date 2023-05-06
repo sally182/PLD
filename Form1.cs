e615ede63e303c81c5d56a54526b5355d504d575
Sally Mohamed Ali <120983369+sally182@users.noreply.github.com>
2023-05-07 01:23:03 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using com.calitha.goldparser;
+
+namespace Projct_plc
+{
+    public partial class Form1 : Form
+    {
+        MyParser p;
+        public Form1()
+        {
+            InitializeComponent();
+            p = new MyParser("project.cgt",listBox1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            p.Parse(textBox1.Text);
+        }
+    }
+}
